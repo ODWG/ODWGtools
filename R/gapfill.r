27886bc5464a3076640d434aeefb08357758a1f1
mkoohafkan <michael.koohafkan@gmail.com>
2019-02-12 16:17:16 -0800

    added the use of `mask` in `gapfill_kalman` rather than assuming NA values are the only pieces to be replaced.

--- R/gapfill.r
+++ R/gapfill.r
@@ -4,20 +4,24 @@
 #' a Kalman filter.
 #'
 #' @param x A vector of data with missing values.
+#' @param mask A logical mask that identifies a subgroup of `x`
+#'  to compute fill values for. Useful when a subset
+#'  of quality-assured data is available. Default action is to
+#'  only fill NA values.
 #' @param ... Additional arguments to [forecast::auto.arima()].
 #' @return The original vector x with missing values imputed.
 #'
 #' @importFrom dplyr if_else
 #' @importFrom stats KalmanRun
 #' @export
-gapfill_kalman = function(x, ...) {
+gapfill_kalman = function(x, mask = is.na(x), ...) {
   if (!requireNamespace('forecast'))
     stop('Could not find package "forecast"')
   fit = forecast::auto.arima(x, ...)
   kr = KalmanRun(x, fit$model)
   newx = sapply(1:length(x), function(i)
     fit$model$Z %*% kr$states[i,])
-  if_else(is.na(x), newx, x)
+  if_else(mask, newx, x)
 }
 
 
