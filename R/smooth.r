a37f13d95672e0fc81a296bde0428cfee1fb8a2a
mkoohafkan <michael.koohafkan@gmail.com>
2019-03-19 14:09:58 -0700

    updated godin smoother to support average, maximum, minimum smooth

--- R/smooth.r
+++ R/smooth.r
@@ -7,14 +7,23 @@
 #'  increment. this consists of two parts: a numeric value defining 
 #'  the size of the increment, and the units of the increment. 
 #'  Acceptable units are "secs", "mins", "hours", "days", or "weeks".
+#' @param Kind what values to filter. Default is the original Godin
+#'  filter which averages the data. Other options are to use the `max`
+#'  or `min` values of each window.
 #' @return A vector of same length as x.
 #'
 #' @details The Godin filter is defined
 #'  \deqn{F = \frac{A_{24}^2}{24^2}\frac{A_{25}}{25}}
 #'
-#' @importFrom RcppRoll roll_mean
+#' @importFrom RcppRoll roll_mean roll_max roll_min
 #' @export
-smooth_godin = function(x, increment) {
+smooth_godin = function(x, increment, kind = c("mean", "max", "min")) {
+  kind = match.arg(kind, c("mean", "max", "min"))
+  roll_fun = switch(kind,
+    "mean" = roll_mean,
+    "max" = roll_max,
+    "min" = roll_min
+  )
   increment = string_to_difftime(increment)
   inc.units = units(increment)
   d25 = as.difftime(25L, units = "hours")
@@ -25,7 +34,7 @@ smooth_godin = function(x, increment) {
   offset = as.numeric(as.difftime(1L, units = "hours"),
     units = inc.units)/as.numeric(increment)
 
-  (roll_mean(lag(x, offset), w24, fill = NA_real_) +
-   roll_mean(lead(x, offset), w24, fill = NA_real_) +
-   roll_mean(x, w25, fill = NA_real_)) / 3
+  (roll_fun(lag(x, offset), w24, fill = NA_real_) +
+   roll_fun(lead(x, offset), w24, fill = NA_real_) +
+   roll_fun(x, w25, fill = NA_real_)) / 3
 }
