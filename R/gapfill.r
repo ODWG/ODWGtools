b3ee4a70f309d2dd6f81fdad1630f94528518332
mkoohafkan <michael.koohafkan@gmail.com>
2019-06-19 15:58:31 -0700

    moved block detection out of gapfill and into separate function

--- R/gapfill.r
+++ R/gapfill.r
@@ -1,3 +1,22 @@
+#' Contiguous Blocks of Discrete Values
+#' 
+#' Identify contiguous periods of discrete values (i.e. QAQC Flags). 
+#'
+#' @inheritParams aggregate_flags
+#' @return A vector of same length as `flags` where each
+#'   element is the size of the block that flag is a part of.
+#'
+#' @examples
+#' flags = sample(c(1L,3L,4L), 10, replace = TRUE)
+#' blocksize(flags)
+#'
+#' @importFrom purrr map
+#' @export
+blocksize = function(flags) {
+  unlist(map(rle(flags)$lengths, ~ rep(.x, .x)))
+}
+
+
 #' ARIMIA and Kalman Filter Gap Fill
 #'
 #' Fill gaps in a timeseries by fitting an ARIMA model and applying
@@ -8,24 +27,20 @@
 #'  to compute fill values for. Useful when a subset
 #'  of quality-assured data is available. Default action is to
 #'  only fill NA values.
-#' @param max.contiguous The maximum number of contiguous values
-#'   to fill.
 #' @param ... Additional arguments to [forecast::auto.arima()].
 #' @return The original vector x with missing values imputed.
 #'
 #' @importFrom dplyr if_else
-#' @importFrom purrr map
 #' @importFrom stats KalmanRun
 #' @export
-gapfill_kalman = function(x, mask = is.na(x), max.contiguous = Inf, ...) {
+gapfill_kalman = function(x, mask = is.na(x), ...) {
   if (!requireNamespace('forecast'))
     stop('Could not find package "forecast"')
   fit = forecast::auto.arima(x, ...)
   kr = KalmanRun(x, fit$model)
   newx = sapply(1:length(x), function(i)
     fit$model$Z %*% kr$states[i,])
-  blocksize = unlist(map(rle(mask)$lengths, ~ rep(.x, .x)))
-  if_else(mask & (blocksize <= max.contiguous), newx, x)
+  if_else(mask, newx, x)
 }
 
 
@@ -34,5 +49,3 @@ gapfill_deep = function(d, mask) {
 
 
 }
-
-
