7094178e8789addc9790fa434368bd2f38710b97
Michael Koohafkan <Michael.Koohafkan@water.ca.gov>
2020-10-27 13:38:43 -0700

    reorganize code

---
+++ R/crossing.r
@@ -0,0 +1,59 @@
+#' @include rtqc.r
+NULL
+
+#' Multivariate test
+#'
+#' Perform a multivariate rate of change test. This function can also be
+#'   used for the neighbor test. See
+#' https://cdn.ioos.noaa.gov/media/2017/12/qartod_temperature_salinity_manual.pdf
+#' for more information.
+#'
+#' @inheritParams rate_test
+#' @param y A vector of coincident observations, e.g. from a separate sensor
+#'   or nearby station.
+#'
+#' @examples
+#' fake.data1 = c(rnorm(10,10), rnorm(10, 50), rnorm(10, 10))
+#' fake.data2 = rep(10,30)
+#' multivariate_test(fake.data1, fake.data2, c(1.5, 3), 5)
+#' multivariate_test(fake.data2, fake.data1, c(1.5, 3), 5)
+#' multivariate_test(fake.data1, fake.data1, 1.5, 5)
+#'
+#' @importFrom dplyr lag lead case_when
+#' @export
+multivariate_test = function(x, y, n.dev, n.prior) {
+  n.dev = rep(n.dev, length.out = 2)
+  n.prior = rep(n.prior, length.out = 2)
+  xf = rate_test(x, n.dev[1], n.prior[1])
+  yf = rate_test(y, n.dev[2], n.prior[2])
+  case_when(
+    is.na(x) ~ NA_integer_,
+    (xf != 1L & yf == 1L) ~ 3L,
+    TRUE ~ 1L
+  )
+}
+
+#' Regional Regression
+#'
+#' Perform regional regression of two stations.
+#'
+#' @param x,y Vectors of station data used for regression.
+#' @param season Optional vector identifying specific 
+#'   seasons or periods
+#' @param plot If `TRUE`, print diagnostic plots.
+#'
+#'
+#'
+#'
+regional_regression = function(x, y, season = NULL, plot = FALSE) {
+  d = data.frame(x = x, y = y)
+  m = lm(y ~ x, d)
+
+  d["yhat"] = predict(m, data = d)
+
+  if(plot){
+    plot(d[1:2], type = "p")
+    lines(d[c(1,3)], col = "blue")
+  }
+
+}
