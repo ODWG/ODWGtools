362ffde778977cd7fe0e7615b242f408c6a2e48e
Michael Koohafkan <Michael.Koohafkan@water.ca.gov>
2021-03-29 07:48:12 -0700

    rtqc tests

---
+++ tests/testthat/test-rtqc.r
@@ -0,0 +1,123 @@
+test_that("gap test works", {
+
+  x = c(seq(as.POSIXct("2018-01-01"), by = "15 mins", length.out = 20))
+  flag.ids = c(NA_integer_, rep(1L, 19))
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_gap(x, "16 mins", "less than"), flags)
+  expect_identical(rtqc_gap(x, "15 mins", "is"), flags)
+
+  x = c(seq(as.POSIXct("2018-01-01"), by = "15 mins", length.out = 20))
+  x = x[-15]
+  flag.ids = c(NA_integer_, rep(1L, 18))
+  flag.ids[15] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_gap(x, "16 mins", "less than"), flags)
+  expect_identical(rtqc_gap(x, "15 mins", "is"), flags)
+
+  flag.ids = c(NA_integer_, rep(1L, 18))
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_gap(x, "31 mins", "less than"), flags)
+
+})
+
+test_that("range test works", {
+
+  x = 5 * sin(seq(1, 10, by = 0.2))
+  flag.ids = rep(1L, length(x))
+  flag.ids[abs(x) > 3] = 3L
+  flag.ids[abs(x) > 4] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_range(x, c(-3, 3), c(-4, 4)), flags)
+
+  flag.ids = rep(1L, length(x))
+  flag.ids[x > 5] = 3L
+  flag.ids[x < 2] = 3L
+  flag.ids[x > 7] = 4L
+  flag.ids[x < 0] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_range(x, c(2, 5), c(0, 7)), flags)
+
+})
+
+
+test_that("alternate spike test works", {
+
+  x = rep(0, 20)
+  spike.locs = c(3, 7, 15)
+  spike.mags = c(10, 20, 30)
+  x[spike.locs] = spike.mags
+
+  flag.ids = c(NA_integer_, rep(1L, 18), NA_integer_)
+  flag.ids[x > 10] = 3L
+  flag.ids[x > 20] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_spike_alt(x, c(10, 20)), flags)
+
+  flag.ids = c(NA_integer_, rep(1L, 18), NA_integer_)
+  flag.ids[x > 15] = 3L
+  flag.ids[x > 25] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_spike_alt(x, c(15, 25)), flags)
+
+  flag.ids = c(NA_integer_, rep(1L, 18), NA_integer_)
+  flag.ids[x > 5] = 3L
+  flag.ids[x > 25] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_spike_alt(x, c(5, 25)), flags)
+
+  flag.ids = c(NA_integer_, rep(1L, 18), NA_integer_)
+  flag.ids[x > 25] = 3L
+  flag.ids[x > 40] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_spike_alt(x, c(25, 40)), flags)
+
+  flag.ids = c(NA_integer_, rep(1L, 18), NA_integer_)
+  flag.ids[x > 40] = 3L
+  flag.ids[x > 50] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(rtqc_spike_alt(x, c(40, 50)), flags)
+})
+
+test_that("alternate rate test works", {
+
+  #rtqc_rate_alt
+
+})
+
+
+test_that("attenuation test works", {
+
+  x = c(rep(c(10, 20), 2), rep(c(10, 5), 2), rep(5, 2))
+  flag.ids = c(NA_integer_, rep(1L, length(x) - 1))
+  flag.ids[6:8] = 3L
+  flag.ids[9:10] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+
+  expect_identical(
+    rtqc_attenuation(x, c(7, 3), 2),
+    flags
+  )
+
+  x = c(rep(c(10, 20), 4), rep(c(10, 5), 4), rep(5, 4))
+  flag.ids = rep(1L, length(x))
+  flag.ids[1:2] = NA_integer_
+  flag.ids[3:9] = 3L
+  flag.ids[11:20] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(
+    rtqc_attenuation(x, c(7, 3), 3),
+    flags
+  )
+
+  x = c(rep(c(10, 20), 4), rep(c(10, 5), 4), rep(5, 4))
+  flag.ids = rep(1L, length(x))
+  flag.ids[1:3] = NA_integer_
+  flag.ids[4:9] = 3L
+  flag.ids[12:20] = 4L
+  flags = ODWGtools:::.rtqc_factor(flag.ids)
+  expect_identical(
+    rtqc_attenuation(x, c(6, 3), 4),
+    flags
+  )
+
+})
