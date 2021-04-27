8a0427ca8e95951cf2b5ad7aa7943d9a3958578d
Michael Koohafkan <Michael.Koohafkan@water.ca.gov>
2021-04-27 10:50:28 -0700

    removed references to BDL data

--- R/ODWGtools.r
+++ R/ODWGtools.r
@@ -6,26 +6,3 @@
 #' @name ODWGtools
 #' @docType package
 "_PACKAGE"
-
-
-#' Belden's Landing Historical Data
-#'
-#' Historical electrical conductivity data collected at 
-#' Belden's Landing.
-#'
-#' @docType data
-#' @keywords datasets
-#' @name bdl
-#' @usage data(bdl)
-#' @format A data frame with 384,771 rows and 3 variables.
-"bdl"
-
-#' Flag Descriptions
-#'
-#' Table of QAQC Flags with descriptions.
-#'
-#' @docType data
-#' @keywords datasets
-#' @name flag.descriptions
-#' @keywords internal
-"flag.descriptions"
